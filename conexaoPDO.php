c5953dd9ee56c6ec8ef892449ce0cb039b6a9fdf
Rogerio Santos <bb.roger@hotmail.com>
2019-04-27 12:20:14 -0300

    commit inicial

--- conexaoPDO.php
+++ conexaoPDO.php
@@ -4,7 +4,7 @@
 
 $dsn = "mysql:dbname=b7web;host=localhost;charset=utf8";//qual banco de dados será utilizado, nome do banco, local do banco
 $dbuser = "root";
-$dbpass = "14725800Ro@";
+$dbpass = "";
 
 try {
 	$pdo = new PDO($dsn, $dbuser, $dbpass);
